29ff3f38387108bd1c593265858da70e0c33a3ef
EmmiAjaX <66840682+EmmiAjaX@users.noreply.github.com>
2020-06-12 22:27:39 +0530

    Add files via upload

---
+++ FirstCode.js
@@ -0,0 +1,8 @@
+var StakeHolder = 100;
+document.write(StakeHolder);
+
+var string_one = "bruh this is a string";
+document.write(string_one);
+
+var Bool_first = true;
+var Bool_second = false;
---
+++ Maths_Operators.js
@@ -0,0 +1,14 @@
+var First_use = 500 / 2;
+document.write(First_use);
+
+var Second_use = 500 * 2;
+document.write(Second_use);
+
+var Third_use = 500 + 2;
+document.write(Third_use);
+
+var Fourth_use = 500 - 2;
+document.write(Fourth_use);
+
+var Fifth_use = 500 % 2;
+document.write(Fifth_use);
